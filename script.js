2a3f340224f5b7ceb88107c4c5e2140c7853eff8
mehdi reko cd <mehdireko74@gmail.com>
2024-07-03 11:35:45 +0330

    new practic object oriented

---
+++ script.js
@@ -0,0 +1,777 @@
+
+/*
+let contryName = "iran";
+let nameOfTheContinent = "asia";
+let numberOfPopulation = "85milion"
+console.log(contryName);
+console.log(nameOfTheContinent);
+console.log(numberOfPopulation);
+*/
+/*let mehdiIsthebestProgrammer = true;
+console.log(mehdiIsthebestProgrammer);
+console.log(typeof mehdiIsthebestProgrammer);
+mehdiIsthebestProgrammer = "mehdo";
+console.log(typeof mehdiIsthebestProgrammer);
+
+let komic;
+console.log(komic);
+console.log(typeof komic);
+ 
+
+console.log(typeof null);
+
+
+//'let isIsland = tr
+//console.log(typeof isIsland);
+//isIsland = "iran";
+//console.log(typeof isIsland);
+//let language;
+//console.log(typeof language)
+let isIsland = "iran"
+let population = 85
+let language = "persion"
+console.log(typeof population)*/
+//const birthday=1995/*
+//birthday =1991
+//l/et age = 31
+//age = 30
+//const firstName= ''
+/*
+const now = 2024
+const birthdayOfMehdi = 1995
+const birthdayofSara = 2000
+const ageMhedi = (now - birthdayOfMehdi)
+const ageSara = (now - birthdayofSara)
+console.log(ageMhedi)
+console.log(ageSara)
+const firstName = "mehdi"
+const lastName = 'rezaei'
+const fullName = (firstName+' '+lastName)
+console.log(fullName)
+console.log(ageMhedi * 2, ageMhedi/10, 2**3)
+
+//Assignment aperation
+
+
+//comparison operators
+/*
+console.log(ageMhedi>ageSara)
+console.log(ageSara>=233)
+console.log(ageMhedi<ageSara)
+console.log(ageMhedi==ageSara)
+console.log(now-1995==ageMhedi)
+console.log(now-ageSara<25)
+*/
+/*
+const mycountry = 'iran'
+const populationInIran = 85
+const halfpopulationIneachhalf = (populationInIran/2)
+console.log(halfpopulationIneachhalf)
+const populationInFinland = 6
+console.log(populationInIran>populationInFinland)
+
+const averageOfaCountry = 33
+console.log(populationInIran<averageOfaCountry)
+//description
+const country= 'portugal is in'
+const whereiscountry = 'europe'
+const numberOfpopulation= 11
+console.log(country+' ' + whereiscountry+' ' +'and'+' ' + numberOfpopulation+'milion population'+'and native speaker are portuaees')
+*/
+/*
+ const marksWeights = 78
+
+ const marksHeight = 1.69
+ const johnWeight = 92
+ const johnHeight = 1.95
+ const marksBmi=(marksWeights/marksHeight**2)
+  console.log(marksBmi)
+ const johnBmi=(johnWeight/johnHeight**2)
+ console.log(johnBmi)
+console.log(marksBmi==johnBmi)
+console.log(marksBmi>johnBmi)
+console
+
+
+console.log(marksBmi,johnBmi, marksBmi>johnBmi)
+*/
+/*
+const marksHeight = 1.69
+const marksWeights=82
+ const johnWeight = 92
+ const johnHeight = 1.95
+ const marksBmi=(marksHeight/marksHeight**2)
+  console.log(marksBmi)
+ const johnBmi=(johnWeight/johnHeight**2)
+ console.log(johnBmi)
+console.log(marksBmi==johnBmi)
+console.log(marksBmi>johnBmi)
+
+if(marksBmi==johnBmi){
+    console.log('that is good for marks');
+} else {
+    console.log('he is long wahy to fix this');
+}
+if(marksHeight>johnHeight){
+    console.log('he is a tall man ')
+}else{
+    console.log('he is smaller man')
+}
+if(marksWeights>johnWeight){
+    console.log("he is fat man")
+}else{
+    console.log('he is sexy man')
+}
+*/
+
+/*challenge3#
+
+const firstTeam = 'Dolphins';
+const secondTeam = 'koalas';
+
+
+const  DolphinsScore = (96 , 108 , 89);
+const koalasScore = (88, 91, 110);
+
+const avgDolphinscore=(96+108+89) / 3;
+const avgKoalasScore=(88+91+110) / 3;
+
+if(avgDolphinscore>avgKoalasScore){
+    console.log('Dolphine just winn game !')
+}else{
+    console.log('koala team are winner')
+}
+
+
+const miniumScore = 100
+ if(avgDolphinscore>miniumScore){
+    console.log('Dolphine just winn game')
+ }else if (avgKoalasScore>miniumScore){
+    console.log('koal are winner')
+
+ }
+ else{
+    console.log('this game are draw')
+ }
+    
+ if(avgDolphinscore>miniumScore){
+    console.log('Dolphin win trophy')
+ }
+ else if(avgKoalasScore>miniumScore){
+    console.log('Koala win trophy')
+ }
+ else{
+    console.log('no team wins trophy')
+ }
+*/
+
+/*
+console.log('2'*'5')
+ console.log(3+'3'+4)
+ const year = '1100'
+ console.log(year)
+ console.log(typeof year)
+ console.log(Number(year)+18);
+ */
+
+//5 falsy values: 0,'',undefined,null,NAN
+/*
+ console.log(Boolean(0));
+ console.log(Boolean(''));
+ console.log(Boolean());
+ console.log(Boolean('mehdi'))
+
+ const englishExam = 1 ; 
+ if(englishExam){
+    console.log('you pass the exam !');
+ }else {
+       console.log('you should pass the exam another time');
+    }
+    */
+/*
+    const age = '18';
+    if(age===18) console.log('you became an adult')
+
+        // we prefer to use 3 equel instead 2 equel remember that
+
+    if(age==18) console.log("string type will type on web bage")
+
+const favorit = Number(prompt('whats your favorit number?'))
+console.log(favorit)
+
+if (favorit===23){
+    console.log('good! 23 is an amazing number')
+}else if(favorit===7){
+    console.log('7 is also good  number')
+}else if (favorit === 9){
+    console.log('9 also is good number')
+}else{
+    console.log('number is not 23 or 7 or 9!')
+}
+*/
+
+//const hasDriversLicense =true ; //A
+//const hasGoodVision = true ; //B/
+/*
+console.log(hasDriversLicense&&hasGoodVision)
+console.log(hasDriversLicense||hasGoodVision)
+
+ console.log(!hasDriversLicense)
+
+ if(shouldDrive){
+    console.log('mehdi is able to Drive!')
+}else {
+    console.log('some one else should Drive...')
+}
+*/
+/*
+const istierd  = true
+
+if (hasDriversLicense && hasGoodVision && !istierd){
+    console.log('mehdi shoule drive !')
+}else{
+    console.log('some one else should drive')
+}
+*/
+/*
+const avereageDolphineSore = (96+108+89)/3
+const averageKoalaScore = (88+50+110)/3
+//////
+if(avereageDolphineSore>100 && avereageDolphineSore>=100){
+    console.log('Dolphine catch the bonus')
+}else if(averageKoalaScore>100 && averageKoalaScore>=100) {
+    console.log('koala win the trophy')
+
+}else if (averageKoalaScore===avereageDolphineSore){
+    console.log('both of team are winner')
+
+ }else{
+    console.log('no one winn trophy !!!')
+ }
+*/
+/*
+ const day = 'wendsday'
+
+ switch (day) {
+ case 'monday':
+    console.log('pelan course structure')
+    console.log('go to coding meetup')
+    break;
+case 'tuseday':
+    console.log('prepare theory videos');
+    break;
+case 'wendsday':
+    console.log('play video game')
+    break;
+case 'thursday':
+    console.log('write code example');
+    break;
+case 'friday' :
+    console.log('record videos')
+    break;
+case 'saturd
+ay':
+case 'sunday':
+    console.log('enjoy the weekend')
+    break;
+    default:
+        console.log ('not valid day')
+ }
+        
+
+
+if(day==='monday'){
+    console.log('pelan course structure')
+    console.log('go to coding meetup')
+}else if(day==='tuseday'){
+    console.log('prepare theory videos');
+}else if(day==='wendsday'){
+    console.log('play video game')
+}else if(day==='thursday'){
+    console.log('write code example')
+}else if(day==='friday'){
+    console.log('write code example');
+}else if(day==='saturday'){
+    console.log('enjoy the weekend')
+}else if(day==='sunday'){
+    console.log('enjoy the weekend')
+}else{
+    console.log('no valid day')
+}*/
+/*  
+let age = 18
+
+let canVote = (age>=18) ? 'yes of course😊' : "no sorry😢";
+console.log(canVote);
+
+let aGe = 15
+let driveThecar=(aGe>=18) ? 'yes you can drive the car😍' : 'no you cant drive the car untill you grow up❤️'
+console.log(driveThecar);
+//section 1
+let x=5
+let reult = (x>=5) ? ' grater than 5' : 'smaller than 5'
+console.log(reult)
+
+//section2
+let tempreture = 30;
+let temp= (tempreture>=25) ? 'the weather is hot🥵' : 'the wether is cool😎😎'
+console.log(temp)
+   //section3
+let isloggin = false;
+let massage = (isloggin===!true)  ?  'welcome' : 'please loggin'
+console.log(massage)
+
+let sen = 18
+let gender = (sen < 18) ? ' you are young' : 'you are adult'
+           
+   console.log(gender);
+
+let num = -3 
+let number = (num===0) ? 'zero' :
+           (num>0) ? 'positive' : 'negative' ;
+           console.log (number);
+
+ 
+let isnumber = false;
+let logginMessage = (isnumber) ? 'welcome user' : 'please loggin';
+console.log(logginMessage);
+
+
+let isMember = false;
+let welcomeMessage = (isMember) ? 'خوش آمدید عضو' : 'لطفاً عضو شوید';
+console.log(welcomeMessage);
+
+let day = 'شنبه';
+let dayType = (day === 'جمعه') ? 'تعطیل' : 'روز کاری';
+console.log(dayType);
+*/
+
+/*
+let billValue = 100;
+let tip = (billValue <50 || billValue>300 ) ? (billValue*0.2) : (billValue*.15)
+let bill = (billValue <50 || billValue>300 ) ? (billValue-billValue*0.2) : (billValue-billValue*.15)
+
+console.log(`your tip is ${tip} and your bill is ${bill} 💵💵`)
+*/
+/*
+'use strict';
+
+function logger() {
+    console.log('im mehdi rezaei');
+}
+//calling / runnig / invoking function
+logger();
+logger();
+logger();
+
+
+function fruitProssesor(apples, oranges) {
+    return fruitProssesor = `juice with ${apples} appels and ${oranges} oranges.`;
+}
+
+const appleJuice = fruitProssesor(5, 2)
+console.log(appleJuice);
+
+
+/*
+function plusTonumber(Number1 , Number2){
+    const number = `plus ${Number1} and ${Number2} is = ${Number1+Number2}`;
+    return number
+}
+
+const rezult = plusTonumber(5,8)
+console.log(rezult)
+*/
+/*
+
+function greet(name) {
+    return greet = `hello ${name} how are you?`
+}
+let greeting = greet('mehdi')
+console.log(greet)
+
+
+
+function calAge(birthday) {
+    return 2024 - birthday;
+}
+
+const age = calAge(1995)
+console.log(age)
+
+
+
+
+function calAge1(birtyear) {
+    const age = 2024 - birtyear
+    return age
+}
+const age1 = calAge1(1995)
+
+console.log(age1)
+
+
+const calage2 = function (birthday1) {
+    return 2024 - birthday1
+}
+const age3 = calage2(1995);
+console.log(`your age is ${age3} years old`);
+
+const addNumber = function (number1, number2) {
+    return number1 + number2
+}
+const add2number = addNumber(5, 3)
+console.log(`add two number in this function is ${add2number}`)
+
+const rectangle = function (length, width) {
+    return 2 * (length + width)
+}
+const rectangleArea = rectangle(3, 5)
+console.log(`the rectangleArea is ${rectangleArea}`);
+
+const numberOfstring = function (string) {
+    return string.length;//444
+}
+const lengthofString = numberOfstring("mehdi rezaei kochaksaraei")
+console.log(`The given string contains ${lengthofString} letters`);
+
+const convertdegree = function(c) {
+    return 32+(9/5)*c;
+}
+const degreeConvertor = convertdegree(25 );
+console.log(`temperture is ${degreeConvertor} fahrenheit degree`);
+
+
+const equelDtring = function(a,b){
+    return a===b
+}
+console.log(equelDtring('hello', 'hello'))
+*/
+//arrow function
+/*
+const age = birthyear => 2024- birthyear
+const age1 = age(1995)
+console.log(age1)
+
+
+const yearUntilretirement = (birthyear , firstname)=>{
+    const age = 2024 - birthyear
+    const retirement = 65-age
+    return `${firstname} retirement in ${retirement} years`
+}
+console.log(yearUntilretirement(1995, 'mehdi'))
+
+const equalstring = (str1 , str2) =>{
+    return str1===str2
+}
+console.log(equalstring('mehdi' , 'hello'))
+
+
+
+
+const degreeoffarenheight = (C) =>
+    32+(9/5)*C
+console.log(degreeoffarenheight(20))
+
+
+
+const stringNumber = (str) =>
+    str.length 
+console.log(stringNumber('mehdi'))
+
+
+const rectangle =function(length , width){
+    return 2*(length+width);
+}
+console.log(rectangle(5 ,5))
+
+
+const addnumber = function(a , b){
+    return a+b
+}
+console.log(addnumber(2,3))
+
+
+const add2number = (a,b) =>
+    a+b
+console.log(add2number(2 , 5))
+*/
+/*
+function cutfruitpieces(fruit){
+    return fruit*4
+}
+
+
+
+
+function fruitProcesors(apple , oranges){
+    const applepieces = cutfruitpieces(apple)
+    const orangespieces = cutfruitpieces(oranges)
+
+
+    const juice = `juice with ${applepieces} piece of apples and ${orangespieces}piece of oranges`
+    return juice
+}
+
+console.log(fruitProcesors(2,3));
+
+
+const convertor = function(c , f){
+ const centiTofarenheit = (c*1.8)+32
+ const farenheitTocenti =(f-32)/1.8
+ const temp = `the farenheit tempreture is ${centiTofarenheit} and the centigrad temp is ${farenheitTocenti}`
+ return temp
+}
+
+
+ function circlearea(raduis){
+    return 3.14*(raduis*raduis)
+ }
+
+ function circlecalculatin(r){
+    return circlearea(r)
+ }
+
+ console.log(circlecalculatin(5))
+
+ function cal
+ */
+/*
+ function avgDolphinScore (){
+    const dolphineScore = (44+23+71+65+54+49)/6
+    return dolphineScore
+ }
+const res0 = avgDolphinScore()
+
+
+ function avgKoalaScore (){
+    const KoalaScore = (85+54+41+23+34+27)/6
+    return KoalaScore
+ }
+
+const res = avgKoalaScore()
+*/
+/*
+//test of function
+function test () {
+   const testr = 25+2
+    return  testr
+}
+
+const testb = test()
+console.log(testb)  
+
+
+const calcAverageDolphinset1 = () => +(44+23+71)/3
+console.log(calcAverageDolphinset1())
+const calcAveragekoalaset1 = () => +(65+54+49)/3
+console.log(calcAveragekoalaset1())
+
+
+const calcAverageDolphinset2 = () => +(85+54+41)/3
+console.log(calcAverageDolphinset2())
+const calcAveragekoalaset2 = () => +(23+34+27)/3
+console.log(calcAveragekoalaset2())
+
+function calcDolphinese1and2 (a ,b ){
+    const sumOfs1and2 = calcAverageDolphinset1(a) + calcAverageDolphinset2(b)
+    return sumOfs1and2
+}
+const res = calcDolphinese1and2(1,1)
+console.log(`sum of koala set1 and set2 is ${res}`)
+    
+
+
+function calcKoalase1and2 (a ,b ){
+    const sumOfs1and2 = calcAveragekoalaset1(a) + calcAveragekoalaset2(b)
+    return sumOfs1and2
+}
+const res1 = calcKoalase1and2(1,1)
+console.log(`sum of koala set1 and set2 is ${res1}`)
+
+
+function checkwinner () {
+    if(res>= res1){
+        return `the dolphin win the game 🏆`
+    }else if(res1 >= res){
+        return `the koala win the game 🏆`
+    } else {
+        return `the game was draw`
+    }
+}
+    const winner = checkwinner()
+    console.log(winner)
+
+*/
+
+//push methde
+/*
+newFunction();
+function newFunction() {
+    const friends = ['medi', 'dfg', 'dfgfgg'];
+    const newLength = friends.push('mewhdi');
+
+    friends.pop();
+    const popped = friends.pop();
+    console.log(popped);
+
+    friends.includes('mehdi');
+    const mmkk = friends.includes('mehdi');
+    console.log(mmkk);
+
+    if (friends.includes('dfg')) {
+        console.log('your name is mehdi');
+    } else {
+        console.log('you arent mrhfi');
+    }
+
+
+    friends.shift();
+    console.log(friends);
+
+    friends.unshift();
+    console.log(friends);
+
+
+    console.log(friends.indexOf('dfgfgg'));
+    console.log(friends.indexOf('medi'));
+
+    friends.push(23);
+    console.log(friends.indexOf(23));
+
+
+    console.log(friends);
+
+    friends.shift();
+    console.log(friends);
+
+    console.log(friends);
+
+    friends.unshift();
+    console.log(friends);
+}
+*/
+/*
+'use strick'
+const billFormol = () =>
+    bill * 0.15
+
+
+
+function calcBill (bill){
+  const billF = bill-(bill*0.15) 
+  const billF1 = bill-(bill*0.20)
+  const billF2 = (bill*0.150)
+  const billF3 = (bill*0.20)
+
+    if(bill>50 && bill<300){
+        return `your bill is ${billF} and your tip is ${billF2} ===> total bill is ${billF+billF2}`
+    } else{
+        return `yourbill is ${billF1} and your tip is ${billF3} ===> total bill is ${billF+billF2}`
+    }
+    
+}
+
+const calctotal = calcBill(100)
+console.log(calctotal)
+
+const bills = [125 , 555 , 44]
+
+
+const tips = (bills[0])
+const tips1 = (bills[1])
+const tips2 = (bills[2])
+const tota = calcBill(tips)
+const tota1 = calcBill(tips1)
+const tota2 = calcBill(tips2)
+console.log(tota)
+console.log(tota1)
+console.log(tota2)
+*/
+ /*
+const mehdi = {
+    firstname : 'mehdi',
+    lastname : 'rezaei',
+    age : 2024-1995 ,
+    job : 'web development',
+    friends : ['amin','ali' ,'mehrnam']
+};
+console.log(mehdi);
+console.log(mehdi.lastname)
+
+const mm = 'name'
+console.log (mehdi['first'+mm])
+
+
+
+
+mehdi.location = 'iran'
+mehdi['twitter'] = '@mehdireko74'
+console.log(mehdi) 
+
+
+console.log(mehdi.friends.length)
+
+console.log(`i'am ${mehdi.firstname}${mehdi.lastname} and my job
+     is ${mehdi.job} and i have ${mehdi.friends.length} friends and my friends name are
+      ${mehdi.friends} and my first friend name is  ${mehdi.friends[0]}`)
+
+*/
+
+const mehdi = {
+    firstname : 'mehdi',
+    lastname : 'rezaei',
+    birthyear : 1995 ,
+    job: 'webdev',
+    friend: ['ami' , 'affg' , 'sdfg'],
+    hasdriverLicense: true,
+
+    calcAge:function(){
+        this.age=2024-this.birthyear
+        return this.age;
+    },
+
+    getsummary:function(){
+        return `${this.firstname} is ${this.calcAge()} 
+        -years old ${mehdi.job} , and he has
+         ${this.hasdriverLicense ? 'a' : 'no'} driver's license`
+    }
+};
+console.log(mehdi.calcAge());
+console.log(mehdi.age)
+console.log(mehdi.getsummary())
+
+
+const mark = {
+    firstname:"mark",
+    weight: 78,
+    tall:1.69,
+    
+    calcMBmi:function(){
+        this.bmi=this.weight/this.tall**2
+        return this.bmi
+    }
+};
+// console.log(mark.calcMBmi())
+let num = (mark.calcMBmi())
+console.log(`bmi for mark is ${num.toFixed(2)}`)
+
+
+const john = {
+    firstname:"mark",
+    weight: 92,
+    tall:1.95,
+
+    calcJBmi:function(){
+        this.bmi=this.weight/this.tall**2
+        return this.bmi
+    }
+};
+// console.log(john.calcJBmi())
+let num1 = (john.calcJBmi())
+console.log(`bmi for john is ${num1.toFixed(2)}`)
+
+
+const mogh = mark.calcMBmi()>john.calcJBmi() ? `"MARK'S BMI is (${num.toFixed()}) and is higher than JOHN (${num1.toFixed()})!"` :
+ `john bmi is ${num1.toFixed()} and is higher than mark ${num.toFixed()}`  ;
+console.log(mogh)
